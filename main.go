3331149fe1941e5c46ec9425dffad0cd2648a763
Samer Masterson <samer@samertm.com>
2015-07-19 01:45:23 -0700

    Show other people in the group

--- main.go
+++ main.go
@@ -28,9 +28,13 @@ type errorTemplateVars struct {
 }
 
 var baseContext = pongo2.Context{
-	"AbsoluteURL":        AbsoluteURL,
-	"GroupURL":           GroupURL,
-	"GroupShareURL":      GroupShareURL,
+	"AbsoluteURL":   AbsoluteURL,
+	"GroupURL":      GroupURL,
+	"GroupShareURL": GroupShareURL,
+	"GetGroupUsers": func(g Group) []User {
+		us, _ := GetGroupUsers(g)
+		return us
+	},
 	"ShortSHA":           ShortSHA,
 	"CommitMessageTitle": CommitMessageTitle,
 }
