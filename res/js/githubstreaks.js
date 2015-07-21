c874df2759c182499d7033bb981e6c16acce3ff8
Samer Masterson <samer@samertm.com>
2015-07-21 01:49:03 -0700

    make it slightly prettier!

--- res/js/githubstreaks.js
+++ res/js/githubstreaks.js
@@ -1,6 +1,17 @@
 // SAMER: Idiomatic way of doing this?
 // SAMER: Figure out how to use modules :V
 
+function fixFooterPosition(){
+  var height = window.innerHeight;
+  var container = document.getElementById("content");
+  var footerHeight = document.getElementById("footer").clientHeight;
+  if( container.clientHeight + footerHeight > height ){
+    container.style.paddingBottom = "0px";
+  }else{
+    container.style.paddingBottom = (height-container.clientHeight-footerHeight) + "px";
+  }
+}
+
 // Routes
 
 function getGID() {
@@ -11,6 +22,7 @@ function getGID() {
 }
 
 window.onload = function() {
+  fixFooterPosition();
   // Refresh group commits button.
   $('#refresh').click(function(e) {
     console.log("Refreshing group data.");
