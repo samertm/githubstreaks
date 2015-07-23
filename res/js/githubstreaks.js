253cfc47c25601ceb5e477091713f30c4db99a59
Samer Masterson <samer@samertm.com>
2015-07-23 00:52:15 -0700

    Show day as relative (e.g. "two days ago")

--- res/js/githubstreaks.js
+++ res/js/githubstreaks.js
@@ -54,6 +54,47 @@ $(document).ready(function() {
     $(this).select();
   });
 
+  // Set changes tags.
+  var Changes = React.createClass({
+    render: function() {
+      return <span className="changes">
+        <span className="additions">+ {this.props.additions}</span>
+        <span> / </span>
+        <span className="deletions">- {this.props.deletions}</span>
+        </span>;
+    },
+  });
+  $('[data-component="changes"]').each(function(i, e) {
+    React.render(
+        <Changes
+      additions={e.getAttribute("data-additions")}
+      deletions={e.getAttribute("data-deletions")}/>,
+      e
+    );
+  });
+  var DayBar = React.createClass({
+    render: function() {
+      var today = moment();
+      var day = moment(this.props.day, "YYYY-MM-DD");
+      var dayText;
+      if (today.year() == day.year() &&
+          today.dayOfYear() == day.dayOfYear()) {
+        dayText = "today";
+      } else {
+        dayText = day.fromNow();
+      }
+      return <span className="day-link">{dayText}</span>;
+    },
+  });
+  $('[data-component="day-bar"]').each(function(i, e) {
+    React.render(
+        <DayBar
+      day={e.getAttribute("data-day")}/>,
+      e
+    );
+  });
+
+
   // Format commit groups.
   // BLAH @_@
   function formatCommitGroups() {
@@ -91,23 +132,4 @@ $(document).ready(function() {
       }
     }
   }
-
-  // Set changes tags.
-  var Changes = React.createClass({
-    render: function() {
-      return <span className="changes">
-        <span className="additions">+ {this.props.additions}</span>
-        <span> / </span>
-        <span className="deletions">- {this.props.deletions}</span>
-      </span>;
-    },
-  });
-  $('[data-component="changes"]').each(function(i, e) {
-    React.render(
-        <Changes
-      additions={e.getAttribute("data-additions")}
-      deletions={e.getAttribute("data-deletions")}/>,
-      e
-    );
-  });
 });
